63fe82e5d4b7a6c102ef9c453e151aa1f6f5e6d6
audibleblink <domain@me.com>
2022-02-01 12:56:55 -0800

    expose more information in walker
      - PEB is accessible within Walk
      - adds docstrings
      - use Dll struct as input to findDll; allows for populating more info
        that will be returned to the user.

--- dllinquent.go
+++ dllinquent.go
@@ -1,3 +1,5 @@
+// dllinquent provides the ability to search through loaded modules and functions
+// withing a process' PEB
 package dllinquent
 
 import (
@@ -7,6 +9,8 @@ import (
 	"golang.org/x/sys/windows"
 )
 
+// FindInProcesses will enumerate all current process, searching for provided function and returns a map of Process
+// structs as keys and Dll structs as keys
 func FindInProcesses(dllName, funcionName string) (funcAddrs map[memutils.WindowsProcess]Dll, err error) {
 	funcAddrs = make(map[memutils.WindowsProcess]Dll)
 	processes, err := memutils.Processes()
@@ -24,21 +28,29 @@ func FindInProcesses(dllName, funcionName string) (funcAddrs map[memutils.Window
 	return
 }
 
+// FindInProcess will walk the PEB of a given process and search for the provided dll name and function.
+// Dll names must end with '.dll' and functionName is case-sensitive
 func FindInProcess(pid int, dllName, functionName string) (dll Dll, err error) {
-	dll, err = findDll(pid, dllName)
+
+	dll = Dll{
+		DllBaseName: dllName,
+		FuncName:    functionName,
+	}
+
+	err = findDll(pid, &dll)
 	if err != nil {
 		return
 	}
 
-	dll.FuncAddress, err = windows.GetProcAddress(windows.Handle(dll.DllBaseAddr), functionName)
 	return
 }
 
+// FindInSelf delegates to FindInProcess, passing its own PID
 func FindInSelf(dllName, functionName string) (dll Dll, err error) {
 	return FindInProcess(0, dllName, functionName)
 }
 
-func findDll(pid int, dllname string) (dll Dll, err error) {
+func findDll(pid int, dll *Dll) (err error) {
 
 	walker, err := NewPebWalker(pid)
 	if err != nil {
@@ -46,11 +58,19 @@ func findDll(pid int, dllname string) (dll Dll, err error) {
 	}
 
 	for walker.Walk() {
-		currentDllName := strings.ToLower(walker.Dll().DllBaseName)
-		dllname = strings.ToLower(dllname)
+		currentDllName := strings.ToLower(walker.Dll().DllFullName)
+		dll.DllBaseName = strings.ToLower(dll.DllBaseName)
 
-		if strings.HasSuffix(currentDllName, dllname) {
-			dll = walker.Dll()
+		if strings.HasSuffix(currentDllName, dll.DllBaseName) {
+			currDll := walker.Dll()
+			dll.DllFullName = currDll.DllFullName
+			dll.DllBaseAddr = currDll.DllBaseAddr
+			dll.FuncAddress, err = windows.GetProcAddress(windows.Handle(dll.DllBaseAddr), dll.FuncName)
+			if err != nil {
+				return
+			}
+
+			dll.FuncOffset = uint64(dll.FuncAddress) - dll.DllBaseAddr
 			return
 		}
 	}
--- pebwalker.go
+++ pebwalker.go
@@ -23,24 +23,34 @@ type LdrDataTableEntry64 struct {
 	HashLinks                  [16]byte // increase by PVOID+ULONG if <OS6.2
 }
 
+// Dll serves as a representation of the currently iterated module in a PebWalker.
+// It exposes the raw LdrDataTableEntry should the user wish to access additional
+// information.
 type Dll struct {
+	DllFullName string
 	DllBaseName string
 	DllBaseAddr uint64
+	FuncName    string
 	FuncAddress uintptr
+	FuncOffset  uint64
 
 	LdrDataTableEntry LdrDataTableEntry64
 }
 
+// PebWalker create a ioutil.Scanner-like interface for walking loaded modules in
+// a process' PEB
 type PebWalker struct {
-	err      error
+	// PEB holds the PEB for the process provided to NewPebWalker
+	PEB      windows.PEB
+	handle   windows.Handle
 	head     LdrDataTableEntry64
 	current  LdrDataTableEntry64
 	fullName string
 	baseAddr uint64
-	handle   windows.Handle
-	peb      windows.PEB
+	err      error
 }
 
+// NewPebWalker creates a new PebWalker from the provided PID
 func NewPebWalker(pid int) (pw PebWalker, err error) {
 	pw = PebWalker{}
 
@@ -50,16 +60,49 @@ func NewPebWalker(pid int) (pw PebWalker, err error) {
 		return
 	}
 
-	pw.peb, err = memutils.GetPEB(pw.handle)
+	pw.PEB, err = memutils.GetPEB(pw.handle)
 	if err != nil {
 		return
 	}
 
 	pw.head = LdrDataTableEntry64{
-		InMemoryOrderLinks: pw.peb.Ldr.InMemoryOrderModuleList,
+		InMemoryOrderLinks: pw.PEB.Ldr.InMemoryOrderModuleList,
 	}
 
 	return pw, err
+}
+
+// Walk returns true as long as there is Flink (Forward Link) in the Linked List
+// of loaded modules
+func (pw *PebWalker) Walk() bool {
+
+	pw.current = pw.next()
+
+	if pw.err != nil {
+		return false
+	}
+
+	if pw.fullName == "" {
+		pw.err = io.EOF
+		return false
+	}
+
+	return true
+}
+
+// Dll return a repreesentation of the currently iterated module
+func (pw PebWalker) Dll() Dll {
+	return Dll{
+		DllFullName:       pw.fullName,
+		DllBaseAddr:       pw.baseAddr,
+		LdrDataTableEntry: pw.current,
+	}
+}
+
+// Err returns the error that broke out of the Walk loop. If the list is exhausted,
+// Err returns io.EOF
+func (pw PebWalker) Err() error {
+	return pw.err
 }
 
 func (pw *PebWalker) next() (head LdrDataTableEntry64) {
@@ -77,8 +120,8 @@ func (pw *PebWalker) next() (head LdrDataTableEntry64) {
 
 	pw.current = pw.head
 
-	if pw.head.BaseDllName.Length > 0 {
-		pw.fullName, err = memutils.PopulateStrings(pw.handle, &pw.head.BaseDllName)
+	if pw.head.BaseDllName.Length != 0 {
+		pw.fullName, err = memutils.PopulateStrings(pw.handle, &pw.head.FullDllName)
 		if err != nil {
 			pw.err = err
 			return
@@ -91,32 +134,4 @@ func (pw *PebWalker) next() (head LdrDataTableEntry64) {
 
 	return pw.head
 
-}
-
-func (pw *PebWalker) Walk() bool {
-
-	pw.current = pw.next()
-
-	if pw.err != nil {
-		return false
-	}
-
-	if pw.fullName == "" {
-		pw.err = io.EOF
-		return false
-	}
-
-	return true
-}
-
-func (pw PebWalker) Dll() Dll {
-	return Dll{
-		DllBaseName:       pw.fullName,
-		DllBaseAddr:       pw.baseAddr,
-		LdrDataTableEntry: pw.current,
-	}
-}
-
-func (pw PebWalker) Err() error {
-	return pw.err
 }
